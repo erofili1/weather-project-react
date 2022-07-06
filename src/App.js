78726d5ede7636e847cf6141a01f668faf3cc3b8
erofili1 <erixaberi@gmail.com>
2022-07-06 15:47:16 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
+      <div className='container'>
+        <h1>Weather App</h1>
+        <Weather />
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather</div>;
+  return (
+    <div className="Weather">
+     
+        <h2>
+        Hello from Weather
+        </h2>
+    
+    </div>
+    );
 
 }
