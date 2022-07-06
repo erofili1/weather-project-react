fd08492582f22e9116567748eba9ca7c64c88894
erofili1 <erixaberi@gmail.com>
2022-07-06 15:25:39 +0100

    Created Weather.js & Weather.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Weather App</h1>
+      <Weather />
     </div>
   );
 }
