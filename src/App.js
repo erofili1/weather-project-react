6474f3c1ee8f5038f4a3e7193053ce61823af45d
erofili1 <erixaberi@gmail.com>
2022-07-06 16:39:02 +0100

    basic HTML structure, fake data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
-        <h1>Weather App</h1>
+        <h2>Weather App</h2>
         <Weather />
       </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -4,11 +4,38 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-     
-        <h2>
-        Hello from Weather
-        </h2>
-    
+      <form className="search-form">
+        <div className="row">
+          <div className="col-8">
+            <input className="form-control" type="search" placeholder="Enter a city..." />
+          </div>
+          <div className="col-4">
+            <input className="btn btn-primary" type="submit" value="Search" />
+          </div>
+        </div>
+      </form>
+      <div className="row">
+        <div className="col-5">
+          <ul className="weather-info-list">
+            <li><h2>Chania</h2></li>
+            <li>Wednesday 15:00</li>
+            <li>Sunny</li>
+          </ul>
+        </div>
+        <div className="col-2">
+          <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny" />
+        </div>
+        <div className="col-1">
+          31 °C
+        </div>
+        <div className="col-4">
+          <ul>
+            <li>Percipitation: 0%</li>
+            <li>Humidity: 44%</li>
+            <li>Wind: 5 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
     );
 
