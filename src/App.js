2666a9de519be215a2ff7edcf33613e53cbdccd3
erofili1 <erixaberi@gmail.com>
2022-08-16 16:03:12 +0100

    Updated footer in App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ export default function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          open-sourced on GitHub.
-        </a>
+          open-sourced
+        </a>{" "}
+        on GitHub.
       </footer>
     </div>
   );
