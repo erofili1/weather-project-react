14a2fb50dbebdfd4a0905a74af6808be80ca014b
erofili1 <erixaberi@gmail.com>
2022-08-16 16:01:34 +0100

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          open-sourced on GitHub
+          open-sourced on GitHub.
         </a>
       </footer>
     </div>
