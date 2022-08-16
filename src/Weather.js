0fa4776bd6788de05bb4d0afd87d0ed3defc6ea2
erofili1 <erixaberi@gmail.com>
2022-08-16 15:07:36 +0100

    Deleted SearchForm component and moved code back to Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import SearchForm from "./SearchForm";
 import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   const apiKey = `7654bb3646824703bcfdf4ced8409f03`;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
@@ -24,10 +24,43 @@ export default function Weather(props) {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    alert(`You have selected ${city}.`);
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form className="search-form" onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="col-9">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Enter a city..."
+            autoFocus="on"
+            onChange={handleCityChange}
+          />
+        </div>
+        <div className="col-3">
+          <input
+            className="btn btn-primary w-100"
+            type="submit"
+            value="Search"
+          />
+        </div>
+      </div>
+    </form>
+  );
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <SearchForm city={props.defaultCity} />
+        {form}
 
         <WeatherInfo info={weatherData} />
       </div>
