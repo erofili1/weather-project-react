bfa599cac7a196387cc204fc1e1cfc0f401d7b6f
erofili1 <erixaberi@gmail.com>
2022-08-22 13:52:38 +0100

    Completed WeatherIcon with animated npm package

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,11 +1,32 @@
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
+  const codeMap = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY-DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "PARTLY_CLOUDY-DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "RAIN",
+    "09n": "RAIN",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
+  };
+
   return (
     <ReactAnimatedWeather
-      icon="CLEAR_DAY"
+      icon={codeMap[props.code]}
       color="black"
-      size={26}
+      size={52}
       animate={true}
     />
   );
