6455ce6345806d0fce218e0b63bab518a9e5f457
erofili1 <erixaberi@gmail.com>
2022-10-01 04:04:32 +0100

    Refactored temperature

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,6 +1,15 @@
 import WeatherIcon from "./WeatherIcon";
 
 export default function ForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
   return (
     <div>
       <div className="forecast-day">{props.data.dt}</div>
@@ -8,8 +17,8 @@ export default function ForecastDay(props) {
         <WeatherIcon code={props.data.weather[0].icon} />
       </div>
       <div className="Forecast-temperatures">
-        <span className="temp-max">{Math.round(props.data.temp.max)}°</span>{" "}
-        <span className="temp-min">{Math.round(props.data.temp.min)}°</span>
+        <span className="temp-max">{maxTemperature()}</span>{" "}
+        <span className="temp-min">{minTemperature()}</span>
       </div>
     </div>
   );
