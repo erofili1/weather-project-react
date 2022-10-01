4d426d3d515aea62a360873e98bf2b5fad6c9385
erofili1 <erixaberi@gmail.com>
2022-10-01 05:03:00 +0100

    Removed Fahrenheit link
    Since I'm not showing Fahrenheit in the forecast, I've commented out the code that converts celsius to fahrenheit.

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,41 +1,45 @@
 import { useState } from "react";
 
 export default function Temperature(props) {
-  const [unit, setUnit] = useState("celsius");
+  // const [unit, setUnit] = useState("celsius");
 
-  function fahrenheit() {
-    return Math.round((props.celsius * 9) / 5 + 32);
-  }
+  // function fahrenheit() {
+  //   return Math.round((props.celsius * 9) / 5 + 32);
+  // }
 
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
+  // function showFahrenheit(event) {
+  //   event.preventDefault();
+  //   setUnit("fahrenheit");
+  // }
 
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
+  // function showCelsius(event) {
+  //   event.preventDefault();
+  //   setUnit("celsius");
+  // }
 
-  if (unit === "celsius") {
-    return (
-      <div>
-        <span className="temperature">{Math.round(props.celsius)}</span>{" "}
-        <span className="celsius">°C |</span>{" "}
-        <a href="/" className="fahrenheit" onClick={showFahrenheit}>
+  // if (unit === "celsius") {
+
+  return (
+    <div>
+      <span className="temperature">{Math.round(props.celsius)}</span>{" "}
+      <span className="celsius">
+        <strong>°C</strong>{" "}
+      </span>
+      {/* {" "} */}
+      {/* <a href="/" className="fahrenheit" onClick={showFahrenheit}>
           °F
-        </a>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <span className="temperature">{fahrenheit()}</span>{" "}
-        <a href="/" className="celsius" onClick={showCelsius}>
-          °C |
-        </a>{" "}
-        <span className="fahrenheit">°F</span>
-      </div>
-    );
-  }
+        </a> */}
+    </div>
+  );
+  // } else {
+  //   return (
+  //     <div>
+  //       <span className="temperature">{fahrenheit()}</span>{" "}
+  //       <a href="/" className="celsius" onClick={showCelsius}>
+  //         °C |
+  //       </a>{" "}
+  //       <span className="fahrenheit">°F</span>
+  //     </div>
+  //   );
 }
+// }
